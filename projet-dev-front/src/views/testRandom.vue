83e32cd5903ed9993acb29d0a6ba819c028bdb6c
roulios44 <jules.croizier@gmail.com>
2023-03-28 22:46:05 +0200

    random test

---
+++ projet-dev-front/src/views/testRandom.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div v-for="rand in random">
+            {{ rand }}
+        </div>
+    </div>
+  </template>
+  
+  
+  <script>
+  import axios from 'axios'
+import { getRandomValues } from 'crypto';
+  export default {
+    data(){
+      return{
+        random: [],
+      }
+    },
+    methods:{
+        async getRandom(){
+            axios.post("",{
+                idCategory: 1,
+                quantity: 5,
+            })
+        }
+    },
+    async mounted(){
+        await this.getRandom()
+    },
+    watch:{
+      
+    }
+  }
+  </script>
+  
+  <style>
+
+  </style>
